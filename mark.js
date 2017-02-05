c5da8a6f1a39801e013f4218c0ae7c0b30dbfe66
lon <longhaohe@gmail.com>
2017-02-05 23:52:38 +0800

    Initial project

--- mark.js
+++ mark.js
@@ -1,95 +1,31 @@
-const fs = require('fs')
 const marked = require('./marked')
 
 const LINKED_MARKDOWN_RE = /<script[^>]*>([^]*)<\/script>([^]*)$/
 
-const fsAsync = new Proxy({}, {
-  get: function (target, key, receiver) {
-    if (key === 'constants') {
-      return fs.constants
-    }
-    return function (...args) {
-      return new Promise((resolve, reject) => {
-        fs[key](...args, (error, value) => {
-          if (error) {
-            reject(error)
-          } else {
-            resolve(value)
-          }
-        })
-      })
-    }
-  }
-})
-
-class MarkFile {
+class MarkedPair {
   constructor ({
-    name,
-    path,
-    content,
     linked,
-    output
+    content
   } = {}) {
-    this.name = name
-    this.path = path
-    this.content = content
     this.linked = linked
-    this.output = output
+    this.content = content
+  }
+
+  get output () {
+    return `<script type="application/ld+json">
+${JSON.stringify(this.linked, null, 2)}
+</script>
+
+${this.content}`
   }
 }
 
-class MarkFold {
-  constructor ({
-    name,
-    path,
-    files
-  }) {
-    this.name = name
-    this.path = path
-    this.files = files
-  }
-}
-
-function markContent (raw) {
-  const matches = raw.match(LINKED_MARKDOWN_RE)
+function mark (source) {
+  const matches = source.match(LINKED_MARKDOWN_RE)
   const linked = JSON.parse(matches[1])
-  const output = marked(matches[2])
-  return {linked, output}
-}
-
-async function markAFile (path) {
-  const content = await fsAsync.readFile(path, 'utf8')
-  let markedContent = ''
-  try {
-    markedContent = markContent(content)
-  } catch (error) {
-    console.error('Error when marking file: ', path)
-    throw error
-  }
-  const {linked, output} = markContent(content)
-  const file = new MarkFile({
-    path,
-    content,
-    linked,
-    output
-  })
-  return file
-}
-
-async function mark (path = './') {
-  const stats = await fsAsync.stat(path)
-  if (stats.isDirectory()) {
-    const files = await fsAsync.readdir(path)
-    const markFiles = await Promise.all(files.map(markAFile))
-    const fold = new MarkFold({
-      path,
-      files: markFiles
-    })
-    return fold
-  } else {
-    const file = await markAFile(path)
-    return file
-  }
+  const content = marked(matches[2])
+  const markedPair = new MarkedPair({linked, content})
+  return markedPair
 }
 
 module.exports = mark
